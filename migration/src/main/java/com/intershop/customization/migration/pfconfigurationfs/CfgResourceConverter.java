6fda334f5b9faff3303b34a49391783fd18e9f16
Heiko Mordt <h.mordt@intershop.de>
2025-05-26 17:14:45 +0200

    get changes to recent main branch

--- migration/src/main/java/com/intershop/customization/migration/pfconfigurationfs/CfgResourceConverter.java
+++ migration/src/main/java/com/intershop/customization/migration/pfconfigurationfs/CfgResourceConverter.java
@@ -4,21 +4,24 @@ import java.io.IOException;
 import java.nio.file.Path;
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.HashMap;
 import java.util.List;
 
 import com.intershop.customization.migration.utils.FileUtils;
 import org.slf4j.LoggerFactory;
 
 /**
- * helper class to Convert ICM 7.10 configuration .resource file into .properties file. They are used for
+ * Helper class to Convert ICM 7.10 configuration .resource file to .properties file. They are used for
  * <ul>
- * <li>transport - name *_transport.resource</li>
- * <li>application preferencees - name *_appprfrnce.resource</li>
- * <li>user - name *_usr.resource</li>
- * <li>... w.i.p</li> <7ul> and converted into prooertty files.<br/>
+ *     <li>transport settings - * _transport.resource</li>
+ *     <li>application - * _appprfrnce.resource </li>
+ *     <li>user credentials - * _usr.resource </li>
+ *     <li>manages services - *_mngdsrvc.resource</li> 
+•	   <li> domain preferences - * _dmnprfrnce.resource </li> 
+•	</7ul> and converted into property files.<br/>
  * 
  * <p/>
- * Bacgground: "When using the “Test System Configuration Solution Kit”, a rework is necessary according to
+ * background: "When using the “Test System Configuration Solution Kit”, a rework is necessary according to
  * pf_configuration_fs versions for IS7.10 vs. for ICM11. See
  * <ul>
  * <li>"Cookbook - 7.10 Test System Configuration Solution" Kit vs.</li>
@@ -27,19 +30,40 @@ import org.slf4j.LoggerFactory;
  * The *.resource files need to be migrated to *.properties files and wired in cartridge-specific configuration.xml
  * file."
  * 
- * <p/>
- * <b><u>.resource types</u><b>
- * <p/>
- * <u>usr  -* _transport.resource</u>
- * <p/>
- * <u>usr  -* _usr.resource</u>
- * <p/>
- * <u>usr  -* _usr.resource</u>
- * <p/>
- * 
  */
+
+
 public class CfgResourceConverter
 {
+
+    /**
+     * The resource type to be converted, mapping to the resource file name.
+     * <ul>
+     * <li>transport - name *_transport.resource</li>
+     * <li>application preferencees - name *_appprfrnce.resource</li>
+     * <li>usr - name *_usr.resource</li>
+     * <li>mngdsrvc - name *_mngdsrvc.resource</li>
+     * <li>dmnprfrnce - name *_dmnprfrnce.resource</li>
+     * </ul>
+     */
+    public enum ResourceType
+    {
+        TRANSPORT("transport"), APPLICATION("application"), USR("usr"), MNGDSRVC("mngdsrvc"), DMNPRFRNCE("dmnprfrnce");
+
+        private final String value;
+
+        // Constructor
+        ResourceType(String value)
+        {
+            this.value = value;
+        }
+
+        // Getter method
+        public String getValue()
+        {
+            return value;
+        }
+    }
 
     private Path source;
     private Path target;
@@ -50,6 +74,8 @@ public class CfgResourceConverter
     public static final org.slf4j.Logger LOGGER = LoggerFactory.getLogger(CfgResourceConverter.class);
 
     /**
+     * constructor<br/>
+     * 
      * @param resourceType the type of the resource, e.g. "transport", prefixed as "pfconfigurationfs>transport"
      * @param source the source file to convert
      * @param target the target file to write the converted content
@@ -60,32 +86,20 @@ public class CfgResourceConverter
         this.target = target;
 
         this.resourceType = "";
-        switch(resourceType)
+        // set with a valid resource type, only
+        for (ResourceType type : ResourceType.values())
         {
-            case "transport":
-                this.prefix = "pfconfigurationfs>transport";
+            if (type.getValue().equals(resourceType))
+            {
+                this.resourceType = resourceType;
+                this.prefix = "pfconfigurationfs>" + resourceType;
                 break;
-            case "application":
-                this.prefix = "pfconfigurationfs>appprfrnce";
-                break;
-            case "user":
-                this.prefix = "pfconfigurationfs>usr";
-                break;
-            case "service":
-//                this.prefix = "pfconfigurationfs>mngdsrvc";
-                return;
-            case "domain":
-//                this.prefix = "pfconfigurationfs>dmnprfrnc";
-                return;
-            default:
-                LOGGER.warn("Unknown resource type: {}", resourceType);
-                return;
+            }
         }
-        this.resourceType = resourceType;
     }
 
     /**
-     * onvert a transport configuration
+     * convert am ICM 7.10 reaource configuration to an ICM 11+ property file
      */
     public void convertResource()
     {
@@ -112,38 +126,56 @@ public class CfgResourceConverter
             List<String> lines = FileUtils.readAllLines(source);
             List<String> targetLines = new ArrayList<>();
 
-            if("application".equals(this.resourceType) || "transport".equals(this.resourceType))
+            if (ResourceType.TRANSPORT.getValue().equals(this.resourceType)
+                || ResourceType.APPLICATION.getValue().equals(this.resourceType))
             {
                 targetLines = migrateTransportCfg(lines);
             }
-            else if ("user".equals(this.resourceType))
+            else if (ResourceType.USR.getValue().equals(this.resourceType)
+                     || ResourceType.DMNPRFRNCE.getValue().equals(this.resourceType))
             {
-                targetLines = migrateUserCfg(lines);
+                targetLines = migrateSimpleCfg(lines);
+            }
+            else if (ResourceType.MNGDSRVC.getValue().equals(this.resourceType))
+            {
+                targetLines = migrateManagedServiceCfg(lines);
             }
             else
             {
-                //return;
+                LOGGER.debug("Cannot convert file {}", source);
             }
 
             FileUtils.writeLines(target, targetLines);
+            // at least for local development
+            LOGGER.debug("Convered file {} ==>  {}.", source, target);
         }
         catch(IOException e)
         {
             LOGGER.error("Error reading file: " + source, e);
         }
-        LOGGER.debug("Convered file {} ==>  {}.", source, target);
 
     }
 
-    private ArrayList<String> migrateUserCfg(List<String> lines)
-     {
+    /**
+     * Similar for user credentials and domain preferences
+     * <p/>
+     * The ICm 7.10 configuration:<br/>
+     * #ParameterName# = #Value# InactivityPeriod = 0 <br/>
+     * gets converted to ICM11+:<br/>
+     * pfconfigurationfs>dmnprfrnce>#ParameterName# = #Value#<br/>
+     * pfconfigurationfs>dmnprfrnce>InactivityPeriod = 0
+     * 
+     * @param lines the lines of the source file
+     * @return targetLines the lines of the target file
+     */
+    private ArrayList<String> migrateSimpleCfg(List<String> lines)
+    {
         String targetLine = "";
         ArrayList<String> targetLines = new ArrayList<>();
 
         for (String line : lines)
         {
             line = line.trim();
-            String key = "";
 
             // transport resource file
             if (line.isEmpty() || (line.startsWith("#")))
@@ -153,19 +185,32 @@ public class CfgResourceConverter
             }
             else
             {
-                    targetLine = this.prefix + ">" + line.trim();
-                    targetLines.add(targetLine);
+                targetLine = this.prefix + ">" + line.trim();
+                targetLines.add(targetLine);
             }
         }
         return targetLines;
     }
 
-    private ArrayList<String> migrateTransportCfg(List<String> lines) {
+    /**
+     * similar for configurations of #type# transport and application
+     * <p/>
+     * The ICM 7.10 configuration:<br/>
+     * <br/>
+     * gets converted to ICM11+:<br/>
+     * pfconfigurationfs>#type#>#UrlIdentifier#>#ParameterName# = #Value#<br/>
+     * pfconfigurationfs>#type#>rest>ExternalApplicationBaseURL = https://int-live-connect.roehm.com<br/>
+     * whereby<br/>
+     * #ParameterName# to #Value# for the application determined by <site = processed domain>&#UrlIdentifier#.<br/>
+     * 
+     * @param lines the lones of the source file
+     * @return targetLines the lines of the target file
+     */
+    private ArrayList<String> migrateTransportCfg(List<String> lines)
+    {
         ArrayList<String> targetLines = new ArrayList<>();
 
-
         // Process and write lines to another file
-        String lastKey = "";
         String targetLine = "";
         String cfgDomainDir = source.getParent().toFile().getName();
 
@@ -175,7 +220,6 @@ public class CfgResourceConverter
         for (String line : lines)
         {
             line = line.trim();
-            String key = "";
 
             // transport resource file
             if (line.isEmpty() || (line.startsWith("#")))
@@ -185,61 +229,129 @@ public class CfgResourceConverter
             }
             else
             {
-                if ("user".equals(this.resourceType))
-                {
-                    targetLine = this.prefix + ">" + line.trim();
-                    targetLines.add(targetLine);
-                }
-                else
-                {
-                    String[] entry = line.split("=");
+                String[] entry = line.split("=");
 
-                    // scam inputz lin
-                    String cfgGroup = "";
-                    String cfgKey = "";
-                    String cfgValue = "";
+                // scam inputz lin
+                String cfgGroup = "";
+                String cfgKey = "";
+                String cfgValue = "";
+                if (entry.length == 2)
+                {
+                    cfgKey = entry[0].trim();
+                    cfgValue = entry[1].trim();
+                    if (0 >= cfgKey.indexOf("."))
+                    {
+                        cfgGroup = cfgKey.substring(0, cfgKey.indexOf(".") - 1);
+                        cfgKey = cfgKey.substring(cfgKey.indexOf("."), cfgKey.length()).trim();
+                    }
+                }
+
+                // gather configuration paremeters
+
+                List<String> sourceEentry = Arrays.asList(cfgGroup, cfgKey, cfgValue);
+                if (tartEntry.size() < 3)
+                {
+                    tartEntry.add(sourceEentry.get(2).trim());
+                }
+                if (tartEntry.size() == 3)
+                {
+                    String groupStr = tartEntry.get(0).trim();
+                    if (ResourceType.APPLICATION.getValue().equals(this.resourceType))
+                    {
+                        groupStr = cfgDomainDir + ">" + groupStr;
+                    }
+                    targetLine = this.prefix + ">" + groupStr + ">" + tartEntry.get(1).trim() + " = "
+                                    + tartEntry.get(2).trim();
+                    if (!targetLine.endsWith(" = n/a"))
+                    {
+                        targetLines.add(targetLine);
+                    }
+                    tartEntry = new ArrayList<String>();
+                }
+            }
+        }
+        return targetLines;
+    }
+
+    /**
+     * The ICm 7.10 configuration:<br/>
+     * ConfigItemX.ServiceDefinitionID=...<br/>
+     * ConfigItemX.ServiceConfigurationName=...<br/>
+     * ConfigItemX.ParameterName=...<br/>
+     * ConfigItemX.Value=...<br/>
+     * <br/>
+     * gets converted to ICM11+:<br/>
+     * pfconfigurationfs>mngdsrvc>#ServiceDefinitionID#>#ServiceConfigurationName#>#ParameterName# = #Value#b#<r/>
+     * ...with the 4 values IS7.10
+     */
+    private ArrayList<String> migrateManagedServiceCfg(List<String> lines)
+    {
+        ArrayList<String> targetLines = new ArrayList<>();
+
+        // Process and write lines to another file
+        String targetLine = "";
+        HashMap<String, String> taretEntry = new HashMap<>();
+
+        // String cfgDomainDir = source.getParent().toFile().getName();
+
+        for (String line : lines)
+        {
+            line = line.trim();
+
+            // transport resource file
+            if (line.isEmpty() || (line.startsWith("#")))
+            {
+                targetLine = line;
+                targetLines.add(targetLine);
+            }
+            else
+            {
+                // scan input line
+
+                String cfgGroup = "";
+                String cfgKey = "";
+                String cfgValue = "";
+
+                // gather the source data
+                if (taretEntry.size() < 4)
+                {
+                    // scan the source line
+                    String[] entry = line.split("=");
                     if (entry.length == 2)
                     {
                         cfgKey = entry[0].trim();
                         cfgValue = entry[1].trim();
-                        if ((!"user".equals(this.resourceType)) && (0 >= cfgKey.indexOf(".")))
+                        if (0 >= cfgKey.indexOf("."))
                         {
                             cfgGroup = cfgKey.substring(0, cfgKey.indexOf(".") - 1);
                             cfgKey = cfgKey.substring(cfgKey.indexOf("."), cfgKey.length()).trim();
                         }
                     }
-                    
-                    List<String> sourceEentry = Arrays.asList(cfgGroup, cfgKey, cfgValue);
-                    key = sourceEentry.get(0).trim();
-                    if (sourceEentry.size() == 3)
+                    taretEntry.put(cfgKey, cfgValue);
+                }
+                // all values found - build and add the target line and
+                // reset the source data
+                if (taretEntry.size() == 4)
+                {
+                    if (0 >= cfgKey.indexOf("."))
                     {
-                        if (tartEntry.size() < 3)
-                        {
-                            tartEntry.add(sourceEentry.get(2).trim());
-                        }
-                        if (tartEntry.size() == 3)
-                        {
-                            System.out.println("~~~" + cfgGroup + ", "  + cfgKey + ", " + cfgValue);
-                        
-                            String groupStr = tartEntry.get(0).trim();
-                            if ("application".equals(this.resourceType))
-                            {
-                                groupStr = cfgDomainDir + ">" + groupStr;
-                            }
-                            targetLine = this.prefix 
-                            + ">" + groupStr
-                            + ">" + tartEntry.get(1).trim() 
-                            + " = " + tartEntry.get(2).trim();
-                            if (!targetLine.endsWith(" = n/a"))
-                            {
-                                targetLines.add(targetLine);
-                            }
-                            tartEntry = new ArrayList<String>();
-                        }
+                        cfgKey = cfgKey.substring(cfgKey.indexOf(".") + 1, cfgKey.length());
                     }
+                    cfgGroup = cfgKey.substring(0, cfgKey.indexOf("."));
+                    StringBuffer bTargetLine
+                    = new StringBuffer().append(this.prefix)
+                      .append(">")
+                      .append(taretEntry.get( cfgGroup + ".ServiceDefinitionID"))
+                      .append(">").append(taretEntry.get(cfgGroup + ".ServiceConfigurationName"))
+                      .append(">").append(taretEntry.get(cfgGroup + ".ParameterName"))
+                      .append(" = ").append(taretEntry.get(cfgGroup + ".Value"));
+
+                    targetLine = bTargetLine.toString();
+                    targetLines.add(targetLine);
+
+                    taretEntry.clear();
                 }
             }
-            lastKey = key;
         }
         return targetLines;
     }
