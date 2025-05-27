f81f748fd49639822008e30ad64beaa074f3045b
rowiish <67745292+rowiish@users.noreply.github.com>
2025-05-27 16:33:42 +0200

    doc: add missing JavaDoc; adjust log level

--- migration/src/main/java/com/intershop/customization/migration/gradle/ConvertToKotlin.java
+++ migration/src/main/java/com/intershop/customization/migration/gradle/ConvertToKotlin.java
@@ -18,6 +18,19 @@ import org.slf4j.LoggerFactory;
 
 import com.intershop.customization.migration.common.MigrationPreparer;
 
+/**
+ * This migration step is calls a customized version of the "GradleKotlinConverter"
+ * to convert build.gradle files to build.gradle.kts files.
+ * <p>
+ * Migrated source files are deleted.
+ * <p>
+ * Example YAML configuration:
+ * <pre>
+ * type: specs.intershop.com/v1beta/migrate
+ * migrator: com.intershop.customization.migration.gradle.ConvertToKotlin
+ * message: "refactor: convert 'build.gradle' files to 'build.gradle.kts'"
+ * </pre>
+ */
 public class ConvertToKotlin implements MigrationPreparer
 {
     public static final String GRADLE_KOTLIN_CONVERTER = "kotlin/gradlekotlinconverter.kts";
@@ -27,7 +40,7 @@ public class ConvertToKotlin implements MigrationPreparer
     public void migrate(Path resource, MigrationContext context)
     {
         String resourceName = getResourceName(resource);
-        LOGGER.debug("Starting Kotlin migration for project '{}'", resourceName);
+        LOGGER.info("Migrating project build files of '{}' to Kotlin", resourceName);
 
         String kotlinVersion = getKotlinRuntimeVersion();
         if (kotlinVersion == null)
@@ -38,7 +51,7 @@ public class ConvertToKotlin implements MigrationPreparer
             return;
         }
 
-        LOGGER.info("Kotlin runtime version found: {}", kotlinVersion);
+        LOGGER.debug("Kotlin runtime version found: {}", kotlinVersion);
 
         try
         {
@@ -54,11 +67,21 @@ public class ConvertToKotlin implements MigrationPreparer
         }
     }
 
+    /**
+     * Returns the name of the executable depending on the operating system.
+     *
+     * @return "kotlin" for Unix-based OS or "kotlin.bat" for Windows.
+     */
     private String getKotlinExecutable()
     {
         return OsCheck.isWindows() ? "kotlin.bat" : "kotlin";
     }
 
+    /**
+     * Returns the version of the found Kotlin runtime.
+     *
+     * @return the version of the Kotlin runtime, or <code>null</code> if it could not be determined.
+     */
     private String getKotlinRuntimeVersion()
     {
         try
@@ -85,6 +108,14 @@ public class ConvertToKotlin implements MigrationPreparer
         }
     }
 
+    /**
+     * Executes the Kotlin script to convert the given Gradle build file of the given path.
+     *
+     * @param resource the path of the parent directory to the Gradle build file to convert
+     * @return the collected output
+     * @throws IOException unhandled possible IO exception
+     * @throws InterruptedException unhandled possible interruption exception
+     */
     private String executeKotlinScript(Path resource) throws IOException, InterruptedException
     {
         Path scriptPath = getKotlinScriptPath();
@@ -100,6 +131,16 @@ public class ConvertToKotlin implements MigrationPreparer
         );
     }
 
+    /**
+     * Convenience method to execute a Kotlin process with the given command and process the output.
+     *
+     * @param command array of command line arguments to execute the Kotlin process
+     * @param outputProcessor a bi-function that processes the output of the Kotlin process.
+     * @return the result of the handed in bi-function
+     * @param <T> expected return type of the output processor
+     * @throws IOException unhandled possible IO exception
+     * @throws InterruptedException unhandled possible interruption exception
+     */
     private <T> T executeKotlinProcess(String[] command, BiFunction<Integer, String, T> outputProcessor) throws IOException, InterruptedException
     {
         ProcessBuilder processBuilder = new ProcessBuilder(command);
@@ -121,6 +162,13 @@ public class ConvertToKotlin implements MigrationPreparer
         return outputProcessor.apply(exitCode, output.toString().trim());
     }
 
+    /**
+     * Helper function to resolve the script in the classpath and convert to a usable the path
+     * to hand over to the Kotlin executable.
+     *
+     * @return temporary path a copy of the Kotlin script
+     * @throws IOException unhandled possible IO exception
+     */
     private Path getKotlinScriptPath() throws IOException
     {
         try (InputStream scriptStream = getClass().getClassLoader().getResourceAsStream(GRADLE_KOTLIN_CONVERTER))
